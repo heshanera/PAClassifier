42bc0f316495963bee0065d739fbbade169fa5bd
Heshan Eranga <erangamx@gmail.com>
2018-03-27 13:36:30 +0530

    commit

--- src/classifier.py
+++ src/classifier.py
@@ -1,6 +1,9 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+from preprocessor import *
 
-if __name__ == "__main__":
-    print "Hello World"
+def main():
+#    print(to_number('45'))
+#    read_csv_file('../datasets/breast_cancer_wisconsin_dataset.csv')
+    print(create_input_vector([0], '1,2,3,4,5,6,?,8,9,10'))
+  
+if __name__== "__main__":
+  main()
